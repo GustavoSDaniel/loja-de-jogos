1da97b64d81e64c2e6c3d0b5cc0128465ff1afd6
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-06-19 18:26:29 -0300

    Iniciando o backend do prjeto

---
+++ loja-de-jogos-api/src/main/java/com/gustavosdaniel/loja_de_jogos/LojaDeJogosApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavosdaniel.loja_de_jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeJogosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeJogosApiApplication.class, args);
+	}
+
+}
---
+++ loja-de-jogos-api/src/test/java/com/gustavosdaniel/loja_de_jogos/LojaDeJogosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavosdaniel.loja_de_jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeJogosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
